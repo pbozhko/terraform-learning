e1c78baf33e2be45a0bf74878aab644e2c9800cd
Pavel Bazhko <p.bozhko92@mail.ru>
2021-10-22 23:50:14 +0300

    Create simple AWS instances

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # **Terraform practical lessons**
+
+__How to deploy and clear infrastructure:__
+
+1. `export AWS_ACCESS_KEY=...`
+2. `export AWS_SECRET_ACCESS_KEY=...`
+3. `export AWS_DEFAULT_REGION=...`
+
+
+4. `cd config/`
+5. `terraform init`
+6. `terraform plan`
+7. `terraform apply`
+8. `terraform show`
+9. `terraform destroy`
---
+++ config/resources.tf
@@ -0,0 +1,27 @@
+provider "aws" {}
+
+resource "aws_instance" "adapt_instance_ubuntu" {
+
+  count         = 1
+  ami           = "ami-4d6as54f5as4f6asf46a"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name    = "Test AWS Ubuntu Instance",
+    Owner   = "Pavel Bazhko",
+    Project = "Terraform Lessons"
+  }
+}
+
+resource "aws_instance" "adapt_instance_centos" {
+
+  count         = 2
+  ami           = "ami-0302f3ec240b9d23c"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name    = "Test AWS Centos Instance",
+    Owner   = "Pavel Bazhko",
+    Project = "Terraform Lessons"
+  }
+}

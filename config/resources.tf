5480b45e0297f6819e18f9a60d3ba886c5cb8a1f
Pavel Bazhko <p.bozhko92@mail.ru>
2021-10-24 12:34:33 +0300

    Use dynamic parts of code

--- config/resources.tf
+++ config/resources.tf
@@ -34,22 +34,19 @@ resource "aws_security_group" "my_webserver_sg" {
   name        = "WebServer Security Group"
   description = "My first security group"
 
-  ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"
+  dynamic "ingress" {
+    for_each = [
+      "80",
+      "443"
     ]
-  }
-
-  ingress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
+    content {
+      from_port = ingress.value
+      to_port   = ingress.value
+      protocol  = "tcp"
+      cidr_blocks = [
+        "0.0.0.0/0"
+      ]
+    }
   }
 
   egress {
